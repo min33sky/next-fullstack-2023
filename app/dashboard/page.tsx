d8289651a05cff82b3b0c3bad4aecc35fa53991e
min33sky <min33sky@naver.com>
2023-02-20 18:24:28 +0900

    todo: dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Welcome back {session.user?.name}</h1>
+    </main>
+  );
+}
